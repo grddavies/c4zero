c2c15697e94cb943b425a933e395cf8b36d54fad
Gethin Davies <grd.davies@gmail.com>
2022-01-18 17:57:42 +0000

    __all__ to list of str

--- game/connect/__init__.py
+++ game/connect/__init__.py
@@ -2,11 +2,11 @@ from .connect import ConnectGame, ConnectGameConfig, ConnectGameState, DropPiece
 from .connect_players import HumanConnectPlayer, RandomConnectPlayer, AIConnectPlayer
 
 __all__ = [
-    ConnectGame,
-    ConnectGameConfig,
-    ConnectGameState,
-    DropPiece,
-    HumanConnectPlayer,
-    RandomConnectPlayer,
-    AIConnectPlayer,
+    "ConnectGame",
+    "ConnectGameConfig",
+    "ConnectGameState",
+    "DropPiece",
+    "HumanConnectPlayer",
+    "RandomConnectPlayer",
+    "AIConnectPlayer",
 ]
